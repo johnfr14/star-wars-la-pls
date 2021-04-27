925b7318026a418f629f7d6c1349e61fc094204f
johnfr14 <jonathan.alyra@gmail.com>
2021-04-27 19:47:11 +0200

    a

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Planets from "./components/Planets"
+function App() {
+  
+  return (
+    <section className="container py-5" wfd-id="1">
+      <h1 className="mb-5">Planètes dans l'univer Star Wars</h1>
+      <Planets /> 
+    </section>
+  );  
+}
+
+export default App;
