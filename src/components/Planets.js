1af28bed1cdc9ee8cb25ae248820d3c5ab5481a3
johnfr14 <jonathan.alyra@gmail.com>
2021-04-29 17:55:35 +0200

    push

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -6,24 +6,34 @@ const Planets = () => {
 const [planets, setPlanets] = useState([])
 const [url, setUrl] = useState("https://swapi.dev/api/planets/")
 const planet = []
-
 const handleButtonClick = () => {
-  fetchPlanets(url, setPlanets, setUrl)
+  fetchPlanets(url, setUrl)
 }
 
 useEffect(() => {
-fetchPlanets(url, setPlanets, setUrl)
+ fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("something went wrong")
+      }
+      return response.json()
+    })
+    .then((data) => {
+      setPlanets([...planets, ...data.results])
+    })
+    .catch((error) => {
+      console.error(error.message)
+    })
 }, [url])
 
-console.log(planets)
 for (const elem of planets) {
-  planet.push(<Planet key={elem.name} name={elem.name} pop={elem.population} climat={elem.climate}/>)
-}
+    planet.push(<Planet key={elem.name} name={elem.name} pop={elem.population} climat={elem.climate}/>)
+  }
 
 return(
   <div className="row" wfd-id="2">
     {planet}
-    <button onClick={handleButtonClick} type="button" class="btn btn-dark" wfd-id="14">suivantes</button>
+    <button onClick={handleButtonClick} type="button" className="btn btn-dark" wfd-id="14">suivantes</button>
   </div>
 ) 
 }
--- src/fetchPlanets.js
+++ src/fetchPlanets.js
@@ -1,21 +1,19 @@
-const fetchPlanets = (url, setPlanets, setUrl) => {
+
+const fetchPlanets = (url, setUrl) => {
     fetch(url)
     .then((response) => {
+      console.log(response)
       if (!response.ok) {
         throw new Error("something went wrong")
       }
       return response.json()
     })
     .then((data) => {
-      //console.log(planets)
-      //console.log(data.results)
       setUrl(data.next)
-      setPlanets(data.results)
     })
     .catch((error) => {
       console.error(error.message)
     })
-  
 }
 
 export default fetchPlanets
